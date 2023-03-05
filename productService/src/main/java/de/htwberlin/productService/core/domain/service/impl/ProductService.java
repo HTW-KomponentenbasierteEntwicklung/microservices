14873f4acffc0903a495497249bb8fba133234a4
mylinhdao <mylinh.dao@t-online.de>
2023-03-05 13:46:30 +0100

    add productService again

---
+++ productService/src/main/java/de/htwberlin/productService/core/domain/service/impl/ProductService.java
@@ -0,0 +1,61 @@
+package de.htwberlin.productService.core.domain.service.impl;
+
+import de.htwberlin.productService.core.domain.model.Category;
+import de.htwberlin.productService.core.domain.model.Product;
+import de.htwberlin.productService.core.domain.service.exception.ProductIDNotFoundException;
+import de.htwberlin.productService.core.domain.service.interfaces.IProductRepository;
+import de.htwberlin.productService.core.domain.service.interfaces.IProductService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class ProductService implements IProductService {
+
+    private final IProductRepository productRepository;
+
+    public ProductService(IProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Product updateProduct(UUID id, Product product) throws ProductIDNotFoundException {
+        Product existingProduct = productRepository.getById(id);
+        if (existingProduct == null){
+            throw new ProductIDNotFoundException(id);
+        }
+        BeanUtils.copyProperties(product, existingProduct, "id");
+        return productRepository.save(existingProduct);
+    }
+
+    @Override
+    public void deleteProduct(UUID id){
+        productRepository.deleteById(id) ;
+    }
+
+    @Override
+    public Product getProductById(UUID id) {
+        return productRepository.getById(id);
+    }
+
+    @Override
+    public Iterable<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public List<Product> findProductsByName(String name) {
+        return productRepository.findByName(name);
+
+    }
+    @Override
+    public List<Product> findProductsByCategory(Category category) {
+        return productRepository.findByCategory( category);
+    }
+}
---
+++ productService/src/test/java/de/htwberlin/productService/FinalWebshopApplicationTests.java
@@ -0,0 +1,9 @@
+package de.htwberlin.productService;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalWebshopApplicationTests {
+
+
+}
