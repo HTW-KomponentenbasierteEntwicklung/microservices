54a8337fa9c42d66d5b3a2c6683425a543822155
yasemingross <yagro@gmx.de>
2023-04-04 19:03:55 +0200

    pom.xml java 17 compiler

--- productService/src/test/java/de/htwberlin/productService/FinalWebshopApplicationTests.java
+++ productService/src/test/java/de/htwberlin/productService/FinalWebshopApplicationTests.java
@@ -5,12 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(properties = {
-        "spring.jpa.hibernate.ddl-auto=create-drop",
-        "spring.liquibase.enabled=false",
-        "spring.flyway.enabled=false"
-})
+@SpringBootTest()
 class FinalWebshopApplicationTests {
 
     @Test
